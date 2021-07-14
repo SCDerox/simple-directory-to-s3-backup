cb63ef36e8e196672f2ab3d85bc157b45e0d74fd
Simon Csaba <mail@scderox.de>
2021-07-14 16:26:37 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Simple directory to S3 
+## Features
+* This system will backup folders and upload them to your s3 bucket
+* Every backup is encrypted using [cryptify](https://www.npmjs.com/package/cryptify)
+* Backups can automatically done every hour
+* Backups can automatically be deleted from your S3-Bucket every x days
+
+## Should I use this?
+Probably not tbh. It's quite complicated to set up, and the project only hase a very small and specific use-case. 
+`Why is it called "Simple.." if it's complicated to set up? ~ I am very glad you asked, it's because the code is quite simple and it took not a very long time to develop this "thing".`
+
+## Installation
+1. Clone this repo: `git clone https://github.com/SCDerox/simple-directory-to-s3-backup.git`
+2. Install dependencies `npm ci`
+3. Set up a S3-Bucket on AWS
+4. Create a configuration-file called `config.json` in the cloned directory and change the configure parameters (explained below).
+5. Then start the script as described below. 
+
+## Start the system
+* If you only want to back up once run `npm start` in the cloned directory
+* To ensure that backups are performed hourly, I suggest to use [pm2](https://pm2.keymetrics.io/): `pm2 start index.js`
+
+## Configure
+You can change these parameters in the `config.json` you created earlier.
+* `key`: Password with which the ZIP should be encrypted
+* `prefix`: Optional prefix which should be put before every filenname
+* `path`: Path on your S3-Bucket in which the backup should be saved
+* `enabledHourlyUpload`: If enabled the script will backup your files hourly
+* `limitHoursTo`: Array of strings; Hours to limit the hourly upload to
+* `folders`: Array of the following objects:
+    * `localFolder`: Path to your local folder to back up
+    * `zipFolder`: Path to the backuped folder inside the ZIP
+* `runCommandsBeforeExecution`: Array of commands to run before execution (for example `mysqldump`or something)
+* `bucketID`: ID of your S3-Bucket
+* `accessKeyID`: ID of your access-key
+* `secretAccessKey`: Secret of your access-key
+* `deleteItems`: If enabled, items will be deleted automatically (in the specified folder) if they are older than `daysBeforeDeletion`
+
+
+## How do I decrypt the encrypted file?
+You can simply use the [cryptify](https://www.npmjs.com/package/cryptify) -cli and remove the `.crypt`-extension from the filename.
---
+++ index.js
@@ -0,0 +1,76 @@
+const Zip = require('adm-zip');
+const config = require('./config.json');
+const Cryptify = require('cryptify');
+const schedule = require('node-schedule');
+const aws = require('aws-sdk');
+const fs = require('fs');
+const execSync = require('child_process').execSync;
+
+if (config.enabledHourlyUpload) {
+    schedule.scheduleJob('1 * * * *', function () {
+        if (config.limitHoursTo && config.limitHoursTo.length !== 0) {
+            if (config.limitHoursTo.includes(new Date().getHours().toString())) backup();
+        } else backup();
+    });
+}
+
+const s3bucket = new aws.S3({
+    accessKeyId: config.accessKeyId,
+    secretAccessKey: config.secretAccessKey
+});
+
+
+async function backup() {
+    return new Promise((async resolve => {
+        for (const command of config.runCommandsBeforeExecution) {
+            console.log(`Running ${command}`);
+            try {
+                await execSync(command);
+            } catch (e) {
+                console.warn(`Error executing ${command}`);
+            }
+            console.log(`Finished running ${command}`);
+        }
+        const zip = new Zip();
+        console.log('Backing up...');
+        const date = new Date();
+        const filename = `backup-${config.prefix}-${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}-${date.getHours()}:${date.getMinutes()}.zip`;
+        config.folders.forEach(f => {
+            console.log(`Adding ${f.localFolder} to zip...`);
+            zip.addLocalFolder(f.localFolder, f.zipFolder);
+        });
+        console.log(`Saving zip...`);
+        zip.writeZip(`./tmp/${filename}`, async () => {
+            console.log('Encrypting file..\n');
+            await new Cryptify(`./tmp/${filename}`, config.key).encrypt();
+            console.log('Encrypted successfully, uploading to S3...');
+            const readStream = fs.createReadStream(`./tmp/${filename}`);
+            s3bucket.upload({
+                Bucket: config.bucketID,
+                Key: config.path + '/' + `${filename}.crypto`,
+                Body: readStream
+            }, function (err) {
+                if (err) return console.error(`Error uploading: ${err}`);
+                console.log('Uploaded successfully');
+                fs.unlinkSync(`./tmp/${filename}`);
+            });
+            if (config.deleteItems) {
+                s3bucket.listObjects({Bucket: config.bucketID, Prefix: `${config.path}`}, (err, res) => {
+                    if (err) return;
+                    for (const object of res.Contents) {
+                        if (object.LastModified.getTime() + (config.daysBeforeDeletion * 86400000) < new Date().getTime()) {
+                            console.log(`Deleting ${object.Key}...`);
+                            s3bucket.deleteObject({Bucket: config.bucketID, Key: object.Key}, (err, data) => {
+                                if (err) return console.error(`Error deleting: ${err}`);
+                                console.log(`Successfully deleted`);
+                            });
+                        }
+                    }
+                });
+            }
+
+        });
+    }));
+}
+
+if (config.backupOnStart) backup();
